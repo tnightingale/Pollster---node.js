ee335bb1f383c51386770296aaa5a9c08789bd60
Tom Nightingale <tom@tnightingale.com>
2010-11-21 23:51:43 -0800

    now queries pollster rails app for data to send out to clients

--- pollster.js
+++ pollster.js
@@ -2,10 +2,10 @@
  * MODULES
  *******************************************************************************************/
 var app = require("express").createServer(),    // Express module.
-    sys = require("sys");                       // SYS module
+    sys = require("sys"),                       // SYS module
     //url = require("url"),                     // URL module
     //qs = require("querystring"),              // QUERYSTRING module
-    //http = require("http"),                   // HTTP module (http server and client)
+    http = require("http");                   // HTTP module (http server and client)
     //Client = require("mysql").Client,         // MySQL module
 
 var HOST = "0.0.0.0";
@@ -38,14 +38,34 @@ var active_polls = new function () {
   var callbacks = [];
   
   this.set_question = function (active_poll) {
-    participants = callbacks[active_poll.id] || [];
+    var participants = callbacks[active_poll.id] || [];
+    var data_source = '/' + active_poll.id + '.json';
+    
+    var pollster = http.createClient(3000, HOST);
+    var pollster_req = pollster.request('GET', data_source);
+    pollster_req.end();
+    
     log("AP(" + active_poll.id + ") : Set Q: " + active_poll.question_id);
     
-    while (participants.length > 0) {
-      request = participants.shift();
-      log("Responding (" + request.user + "), Q = " + active_poll.question_id);
-      request.callback(active_poll);
-    }
+    // Bind reactions to RESPONSE event.
+    pollster_req.on('response', function (response) {
+      // TODO: Probably should error check HTTP status code?
+      //console.log('STATUS: ' + response.statusCode);      
+      
+      // REMOVED: setEncoding('utf8')
+      
+      // Bind reactions to DATA event.
+      response.on('data', function (chunk) {
+        // Parse received data.
+        var data = JSON.parse(chunk);
+        
+        while (participants.length > 0) {
+          request = participants.shift();
+          request.callback(data);
+          log("Responding (" + request.user + "), Q = " + active_poll.question_id);
+        }
+      });
+    });
   }
   
   this.query = function (user, req_active_poll, callback) {
